13e7296b14f7d6853585c5f9494bfe690900b39f
Mats Larsen <me@supergrecko.com>
2021-10-10 15:23:41 +0200

    Install stencil sass for styling

--- packages/ds2/stencil.config.ts
+++ packages/ds2/stencil.config.ts
@@ -1,8 +1,12 @@
 import { Config } from '@stencil/core'
+import { sass } from '@stencil/sass'
 import { reactOutputTarget } from '@stencil/react-output-target'
 
 export const config: Config = {
   namespace: 'ds2',
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'dist',
